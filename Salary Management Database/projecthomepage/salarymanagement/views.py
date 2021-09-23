8a5dc4bfb0706d373b30dc725b379372a647e82b
Kaye Engbino <engbinop@gmail.com>
2021-09-23 15:40:28 +0800

    Add data on Employee

--- Salary Management Database/projecthomepage/salarymanagement/views.py
+++ Salary Management Database/projecthomepage/salarymanagement/views.py
@@ -60,12 +60,27 @@ class AddEmployerView(View):
 			return redirect('salarymanagement:dashboard_view')
 		else:
 			return HttpResponse('not valid')
-			
-		
 
 class AddEmployeeView(View):
 	def get(self, request):
 		return render(request, 'addemployee.html')
+
+	def post(self, request):
+		form = EmployerForm(request.POST)
+
+		if form.is_valid():
+			fname = request.POST.get("firstname")
+			lname = request.POST.get("lastname")
+			city = request.POST.get("city")
+			age = request.POST.get("age")
+			cnumber = request.POST.get("contact")
+			employer_id = request.POST.get("employer_id")
+			form = Employee(firstname = fname, lastname = lname, city = city, age = age, contact_num = cnumber, employer_id_id = employer_id)
+			form.save()
+
+			return redirect('salarymanagement:dashboard_view')
+		else:
+			return HttpResponse('not valid')
 
 class AddSalaryView(View):
 	def get(self, request):
